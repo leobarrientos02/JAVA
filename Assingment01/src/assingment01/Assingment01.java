f7fa54a10803b97fa0c8d2201cd4deeb74383612
Leonel Barrientos <leobarrientos02@gmail.com>
2021-03-01 14:19:36 -0500

    Assignment1
    Simple loop to print all odd numbers.

---
+++ Assingment01/src/assingment01/Assingment01.java
@@ -0,0 +1,21 @@
+
+package assingment01;
+
+/**
+ *
+ * @author leoba
+ */
+public class Assingment01 {
+
+    public static void main(String[] args) {
+       
+        // For loop to print all numbers from 0 to 100
+        for( int i=0; i<=100 ; i++ ){
+            // Using an If Statement to only print even numbers
+            if ( i % 2 == 0 ){ 
+                // This line of code prints to the screen
+                System.out.println(i);
+            } // Closes if Statement
+        } //  Closes the For Loop
+    }
+} 
